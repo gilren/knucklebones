08254805ebe3af6ede620b5c459aa443fffe8711
gilren <gillet.ren@gmail.com>
2024-06-26 18:01:42 +0200

    Fix window size

--- conf.lua
+++ conf.lua
@@ -2,8 +2,10 @@ if arg[2] == "debug" then
   require("lldebugger").start()
 end
 
+require "globals"
+
 function love.conf(t)
-    t.window.width = 1280
-    t.window.height = 720
+    t.window.width = Globals.screenWidth
+    t.window.height = Globals.screenHeight
     t.window.title = "Knucklebones"
 end
